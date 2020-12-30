668f8c9c3e6ccfe3ce5ea706614ecf3ac10e8688
ASC1566 <67261899+ASC1566@users.noreply.github.com>
2020-12-30 16:20:06 +0000

    Update main.py

--- main.py
+++ main.py
@@ -4,31 +4,31 @@ import os
 
 import discord
 from discord.ext import commands
-import traceback
-import sys
-from dotenv import load_dotenv
 
-load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client()
+intents = discord.Intents.all()
 
 bot = commands.Bot(
 	command_prefix=". ",
-	case_insensitive=True
+	case_insensitive=True,
+  intents=intents
 )
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'{client.user.name} has connected to Discord!')
+    print(f'{bot.user.name} has connected to Discord!')
 
-@client.event
+@bot.event
 async def on_member_join(member):
+    channels = [
+      bot.get_channel(793480577230569534), #information
+      bot.get_channel(793480577230569535), #server questions
+      bot.get_channel(793480577230569536) #verification
+    ]
     await member.create_dm()
     await member.dm_channel.send(
-        f'Hi {member.name},Welcome to the server! Feel free to go to these channels #🟠information and #🟠server-questions to find out the rules of the server and how to use the server. After going through that, you can verify yourself by going to #🟠verification  to gain access to the channels :ShibaHeart: if you understand and agree to the rules of the server! Enjoy your stay ! ^^'
+        f'Hi {member.name},Welcome to the server! Feel free to go to these channels {channels[0].mention} and {channels[1].mention}# to find out the rules of the server and how to use the server. After going through that, you can verify yourself by going to{channels[2].mention}   to gain access to the channels  if you understand and agree to the rules of the server! Enjoy your stay ! ^^'
     )
 
-
-keep_alive()
-client.run(TOKEN)
+keep_alive(botnt.run(TOKEN)
